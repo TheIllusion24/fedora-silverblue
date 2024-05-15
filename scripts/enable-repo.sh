3167a23bfe4f7d2a8908c30d2668734a3ecc931a
TheIllusion24 <ggfekarc@gmail.com>
2024-05-15 23:26:25 +0800

    fix(scripts): update scripts/enable-repo.sh

--- scripts/enable-repo.sh
+++ scripts/enable-repo.sh
@@ -8,10 +8,10 @@ rpm-ostree install \
     https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 rpm-ostree remove \
-    rpmfusion-free-release \
-    rpmfusion-nonfree-release
 
 rpm-ostree install \
     rpmfusion-free-release \
+    rpmfusion-nonfree-release \
+    --uninstall rpmfusion-free-release \
     rpmfusion-nonfree-release
 
