6a9c90552c7d0ba0afdacfb0add076960dad3dcd
TheIllusion24 <ggfekarc@gmail.com>
2024-05-16 02:28:15 +0800

    fix(scripts-rpm): update scripts/vim-default-editor.sh

--- scripts/vim-default-editor.sh
+++ scripts/vim-default-editor.sh
@@ -4,7 +4,6 @@ set -ouex pipefail
 
 # Replace Nano default editor with VIM default editor
 rpm-ostree override remove \
-    nano-default-editor \
-    --install \ 
+    nano-default-editor --install \
     vim-default-editor
 
