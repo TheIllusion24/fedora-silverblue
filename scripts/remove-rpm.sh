d310049765f374bcc494dad26807d30955156208
TheIllusion24 <ggfekarc@gmail.com>
2024-05-21 12:15:44 +0800

    fix(rpm): do not remove firefox from base image

--- scripts/remove-rpm.sh
+++ scripts/remove-rpm.sh
@@ -5,6 +5,4 @@ set -ouex pipefail
 # Remove RPMs from base image
 rpm-ostree override remove \
     default-fonts-cjk-sans \
-    firefox \
-    firefox-langpacks \
     google-noto-sans-cjk-vf-fonts 
