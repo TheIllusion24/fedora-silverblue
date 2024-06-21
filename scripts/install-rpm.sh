968a8755f59e1e6033ec182615908f8b2c4f87c7
TheIllusion24 <ggfekarc@gmail.com>
2024-06-21 16:45:11 +0800

    feat(rpm): install scrcpy

--- scripts/install-rpm.sh
+++ scripts/install-rpm.sh
@@ -2,10 +2,13 @@
 
 set -ouex pipefail
 
+/tmp/scripts/copr-repo.sh
+
 rpm-ostree install \
     firewall-config \
     gnome-themes-extra \
     lm_sensors \
+    scrcpy \
     setools \
     setroubleshoot \
     usbguard \
