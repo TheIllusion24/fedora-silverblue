cebc5b3b9aaef1a2f79acf3cc8f47aa6d16a7600
TheIllusion24 <ggfekarc@gmail.com>
2024-06-29 18:01:26 +0800

    Revert "feat(rpm): add brave-browser"
    This reverts commit 8824392ebb433a42be788ab02fa991c425ca6f79.

--- scripts/brave-symlink.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-mkdir -p /var/opt
-
-mv /var/opt/brave.com /usr/lib/brave.com
-
-cat > /usr/lib/tmpfiles.d/brave-browser.conf <<EOF
-L /opt/brave.com -  -  -  -  /usr/lib/brave.com
-EOF
--- scripts/install-rpm.sh
+++ scripts/install-rpm.sh
@@ -2,10 +2,7 @@
 
 set -ouex pipefail
 
-/tmp/scripts/rpm-repos.sh
-
 rpm-ostree install \
-    brave-browser \
     firewall-config \
     gnome-themes-extra \
     lm_sensors \
