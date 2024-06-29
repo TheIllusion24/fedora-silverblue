c182fefb84502be839a7ca138cba9f91faca3bbf
TheIllusion24 <ggfekarc@gmail.com>
2024-06-29 18:53:37 +0800

    fix(set-chromium-flags): remove duplicated flags

--- scripts/set-chromium-flags.sh
+++ scripts/set-chromium-flags.sh
@@ -5,10 +5,10 @@ set -oue pipefail
 
 sed -i '/--enable-chrome-browser-cloud-management/d' /etc/chromium/chromium.conf
 
-sed -i 's/FEATURES=""/FEATURES="WebRTCPipeWireCapturer,VaapiVideoEncoder,VaapiVideoDecodeLinuxGL,TouchpadOverscrollHistoryNavigation,CanvasOopRasterization,SplitCacheByNetworkIsolationKey,SplitCodeCacheByNetworkIsolationKey,SplitHostCacheByNetworkIsolationKey,PrefetchPrivacyChanges,IsolateSandboxedIframes,StrictOriginIsolation,PartitionConnectionsByNetworkIsolationKey,PartitionHttpServerPropertiesByNetworkIsolationKey,PartitionSSLSessionsByNetworkIsolationKey,PartitionNelAndReportingByNetworkIsolationKey,EnableCrossSiteFlagNetworkIsolationKey,"/g' /etc/chromium/chromium.conf
+sed -i 's/FEATURES=""/FEATURES="WebRTCPipeWireCapturer,TouchpadOverscrollHistoryNavigation,CanvasOopRasterization,SplitCacheByNetworkIsolationKey,SplitCodeCacheByNetworkIsolationKey,SplitHostCacheByNetworkIsolationKey,PrefetchPrivacyChanges,IsolateSandboxedIframes,StrictOriginIsolation,PartitionConnectionsByNetworkIsolationKey,PartitionHttpServerPropertiesByNetworkIsolationKey,PartitionSSLSessionsByNetworkIsolationKey,PartitionNelAndReportingByNetworkIsolationKey,EnableCrossSiteFlagNetworkIsolationKey,"/g' /etc/chromium/chromium.conf
 
 echo '
 
-CHROMIUM_FLAGS+=" --ozone-platform=wayland --no-pings --disk-cache-dir=/dev/null --extension-content-verification=enforce_strict --extensions-install-verification=enforce_strict --disable-features=PrivacySandboxSettings4,InterestFeedV2,NTPPopularSitesBakedInContent,UsePopularSitesSuggestions,MediaDrmPreprovisioning,AutofillServerCommunication,DisableThirdPartyStoragePartitioningDeprecationTrial,OptimizationHints,OptimizationHintsFetching,OptimizationHintsFetchingAnonymousDataConsent --enable-gpu-rasterization --use-gl=angle --use-angle=gl --enable-zero-copy --video-capture-use-gpu-memory-buffer --enable-native-gpu-memory-buffers"
+CHROMIUM_FLAGS+=" --ozone-platform=wayland --no-pings --disk-cache-dir=/dev/null --extension-content-verification=enforce_strict --extensions-install-verification=enforce_strict --disable-features=PrivacySandboxSettings4,InterestFeedV2,NTPPopularSitesBakedInContent,UsePopularSitesSuggestions,MediaDrmPreprovisioning,AutofillServerCommunication,DisableThirdPartyStoragePartitioningDeprecationTrial,OptimizationHints,OptimizationHintsFetching,OptimizationHintsFetchingAnonymousDataConsent --enable-gpu-rasterization --enable-zero-copy --video-capture-use-gpu-memory-buffer"
 
 ' >> /etc/chromium/chromium.conf
