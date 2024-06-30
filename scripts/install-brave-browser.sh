de7bac71296c33a138e0305db33b4fbfa14166aa
TheIllusion24 <ggfekarc@gmail.com>
2024-06-30 21:02:31 +0800

    test: update install-brave-browser.sh

--- scripts/install-brave-browser.sh
+++ scripts/install-brave-browser.sh
@@ -26,7 +26,7 @@ enabled=1
 autorefresh=1
 baseurl=https://brave-browser-rpm-release.s3.brave.com/x86_64
 gpgcheck=1
-repo_gpgcheck=1
+repo_gpgcheck=0
 gpgkey=https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 EOF
 
