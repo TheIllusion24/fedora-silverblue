9acc424a8f7f52e208b9ece2975f928cb22bab56
TheIllusion24 <ggfekarc@gmail.com>
2024-06-30 23:52:24 +0800

    test: update install-brave-browser.sh

--- scripts/install-brave-browser.sh
+++ scripts/install-brave-browser.sh
@@ -25,9 +25,10 @@ name=Brave Browser
 enabled=1
 autorefresh=1
 baseurl=https://brave-browser-rpm-release.s3.brave.com/x86_64
-gpgcheck=1
-gpgkey=https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 EOF
+
+# Import key
+rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 
 # Now let's install the packages.
 rpm-ostree install brave-browser
